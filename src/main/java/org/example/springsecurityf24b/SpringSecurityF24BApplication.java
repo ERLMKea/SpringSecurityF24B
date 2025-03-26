5730b7bbbd61b58a45fe4035e44b78fab10994ac
erlm <erlm@kea.dk>
2025-03-26 09:14:14 +0100

    Initial commit

---
+++ src/main/java/org/example/springsecurityf24b/SpringSecurityF24BApplication.java
@@ -0,0 +1,13 @@
+package org.example.springsecurityf24b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityF24BApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityF24BApplication.class, args);
+    }
+
+}
