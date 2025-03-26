0be99a4efb8d0572e5e26c56a6b005cb7e2073eb
erlm <erlm@kea.dk>
2025-03-26 10:00:08 +0100

    nu med Customer passwordencoder

--- src/main/java/org/example/springsecurityf24b/config/ProjectSecurityConfig.java
+++ src/main/java/org/example/springsecurityf24b/config/ProjectSecurityConfig.java
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -21,4 +25,13 @@ public class ProjectSecurityConfig {
         DefaultSecurityFilterChain obj = http.build();
         return obj;
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        //var obj = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        //return obj;
+        return new BCryptPasswordEncoder(20);
+        //return NoOpPasswordEncoder.getInstance();
+    }
+
 }
